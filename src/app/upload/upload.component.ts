988aef5ec595b62d5dc88dc99f74781d80b836db
guhao <guhao@qiniu.com>
2017-09-04 09:50:02 +0800

    first commit

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -12,35 +12,35 @@ export class UploadComponent  {
   loading: boolean;
   token: String;
 
-  @Input() multiple: boolean = false;
+  @Input() multiple = false;
   constructor(private http: Http,
               private el: ElementRef) { }
 
-  getToken() : void {
+  getToken() {
     this.loading = true;
     this.http.request('http://localhost:8083/uptoken')
-    .subscribe((res : Response) => {
-      this.token = res.json().uptoken
+    .subscribe((res) => {
+      this.token = res.json().uptoken;
+      this.loading = false;
     });
   }
 
-  upload() : void{
-    let inputEl = this.el.nativeElement.firstElementChild;
+  upload() {
+    const inputEl = this.el.nativeElement.firstElementChild;
     if (inputEl.files.length === 0) { return; };
 
-    let files: FileList = inputEl.files;
+    const files: FileList = inputEl.files;
 
 
     for (let i = 0; i < files.length; i++) {
       const formData = new FormData();
-      //formData.append(files[i].name, files[i]);
       formData.append('file', files[i]);
       formData.append('key', files[i].name)
       formData.append('token', this.token);
       this.loading = true;
       this.http
         .post('http://up-z0.qiniu.com', formData)
-        .subscribe((res : Response) => {
+        .subscribe((res) => {
           this.data = res.json();
           this.loading = false;
         });
